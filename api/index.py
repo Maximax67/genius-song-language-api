ebed739019f376c16cebfc3c805bf0027f8e580e
Belikov Maxim <maximax6767@gmail.com>
2025-01-14 17:36:18 +0200

    refactor: replace aiohttp with requests

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-import aiohttp
+import requests
 import logging
 import os
 
@@ -6,41 +6,23 @@ from fastapi import FastAPI, HTTPException, Query
 from urllib.parse import quote_plus
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
-AIOHTTP_SESSION = None
 GENIUS_API_TOKEN = os.getenv("GENIUS_API_TOKEN")
+HEADERS = {"Authorization": f"Bearer {GENIUS_API_TOKEN}"}
 
 app = FastAPI()
 
-@app.on_event("startup")
-async def startup_event():
-    global AIOHTTP_SESSION
-    if AIOHTTP_SESSION is None:
-        AIOHTTP_SESSION = aiohttp.ClientSession(
-            headers={
-                "Authorization": f"Bearer {GENIUS_API_TOKEN}"
-            }
-        )
 
-
-@app.on_event("shutdown")
-async def shutdown_event():
-    global AIOHTTP_SESSION
-    if AIOHTTP_SESSION:
-        await AIOHTTP_SESSION.close()
-
-
-async def get_song_language(name: str) -> str | None:
-    search_json = await _search(name)
+def get_song_language(name: str) -> str | None:
+    search_json = _search(name)
     search_status = search_json["meta"]["status"]
     if search_status != 200:
         raise Exception(f"Genius search error. Status code: {search_status}")
 
     song = _get_song_section(search_json["response"]["hits"])
     song_id = song["id"]
-    song_json = await _get_song_json(song_id)
+    song_json = _get_song_json(song_id)
 
     get_info_status = song_json["meta"]["status"]
     if get_info_status != 200:
@@ -53,12 +35,11 @@ async def get_song_language(name: str) -> str | None:
     return language
 
 
-async def _search(name: str) -> dict:
-    global AIOHTTP_SESSION
+def _search(name: str) -> dict:
     url = "https://api.genius.com/search?q=" + quote_plus(name)
-    async with AIOHTTP_SESSION.get(url) as res:
-        res.raise_for_status()
-        return await res.json(content_type=None)
+    res = requests.get(url, headers=HEADERS)
+    res.raise_for_status()
+    return res.json()
 
 
 def _get_song_section(sections: dict) -> dict:
@@ -69,23 +50,22 @@ def _get_song_section(sections: dict) -> dict:
     raise LookupError("Song not found")
 
 
-async def _get_song_json(id: int) -> str:
-    global AIOHTTP_SESSION
+def _get_song_json(id: int) -> dict:
     url = f"https://api.genius.com/songs/{id}"
-    async with AIOHTTP_SESSION.get(url) as res:
-        res.raise_for_status()
-        return await res.json(content_type=None)
+    res = requests.get(url, headers=HEADERS)
+    res.raise_for_status()
+    return res.json()
 
 
 @app.get("/")
-async def home():
+def home():
     return "Hello from FastAPI on Vercel!"
 
 
 @app.get("/search")
-async def search(q: str = Query(..., description="Name of the song")):
+def search(q: str = Query(..., description="Name of the song")):
     try:
-        language = await get_song_language(q)
+        language = get_song_language(q)
         return {"language": language}
     except LookupError as e:
         logging.exception(f"Song not found: {q}")
